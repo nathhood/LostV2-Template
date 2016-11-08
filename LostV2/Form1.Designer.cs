dfda485f8e0e947dfd9c7898b5dc1891947cb1f8
nathhood <nathhood287@ed.amdsb.ca>
2016-11-08 13:33:06 -0500

    just need sounds

--- LostV2/Form1.Designer.cs
+++ LostV2/Form1.Designer.cs
@@ -37,19 +37,28 @@
             this.redImage = new System.Windows.Forms.PictureBox();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.teamPickPic = new System.Windows.Forms.PictureBox();
+            this.awpPic = new System.Windows.Forms.PictureBox();
+            this.akPic = new System.Windows.Forms.PictureBox();
+            this.m4pic = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.blueImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.redImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamPickPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.awpPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.akPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m4pic)).BeginInit();
             this.SuspendLayout();
             // 
             // outputLabel
             // 
             this.outputLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.outputLabel.ForeColor = System.Drawing.Color.White;
-            this.outputLabel.Location = new System.Drawing.Point(24, 28);
-            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Location = new System.Drawing.Point(32, 34);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(339, 257);
+            this.outputLabel.Size = new System.Drawing.Size(452, 316);
             this.outputLabel.TabIndex = 2;
             this.outputLabel.Text = "label1";
             // 
@@ -57,10 +66,9 @@
             // 
             this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.redLabel.ForeColor = System.Drawing.Color.White;
-            this.redLabel.Location = new System.Drawing.Point(70, 306);
-            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.redLabel.Location = new System.Drawing.Point(93, 377);
             this.redLabel.Name = "redLabel";
-            this.redLabel.Size = new System.Drawing.Size(257, 23);
+            this.redLabel.Size = new System.Drawing.Size(343, 28);
             this.redLabel.TabIndex = 3;
             this.redLabel.Text = "label1";
             // 
@@ -68,20 +76,19 @@
             // 
             this.blueLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.blueLabel.ForeColor = System.Drawing.Color.White;
-            this.blueLabel.Location = new System.Drawing.Point(70, 342);
-            this.blueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.blueLabel.Location = new System.Drawing.Point(93, 421);
             this.blueLabel.Name = "blueLabel";
-            this.blueLabel.Size = new System.Drawing.Size(257, 23);
+            this.blueLabel.Size = new System.Drawing.Size(343, 28);
             this.blueLabel.TabIndex = 4;
             this.blueLabel.Text = "label1";
             // 
             // blueImage
             // 
             this.blueImage.Image = global::LostV2.Properties.Resources.blue_50x50;
-            this.blueImage.Location = new System.Drawing.Point(27, 332);
-            this.blueImage.Margin = new System.Windows.Forms.Padding(2);
+            this.blueImage.Location = new System.Drawing.Point(36, 409);
+            this.blueImage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.blueImage.Name = "blueImage";
-            this.blueImage.Size = new System.Drawing.Size(39, 32);
+            this.blueImage.Size = new System.Drawing.Size(52, 39);
             this.blueImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.blueImage.TabIndex = 1;
             this.blueImage.TabStop = false;
@@ -89,10 +96,10 @@
             // redImage
             // 
             this.redImage.Image = global::LostV2.Properties.Resources.red_50x50;
-            this.redImage.Location = new System.Drawing.Point(27, 296);
-            this.redImage.Margin = new System.Windows.Forms.Padding(2);
+            this.redImage.Location = new System.Drawing.Point(36, 364);
+            this.redImage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.redImage.Name = "redImage";
-            this.redImage.Size = new System.Drawing.Size(39, 32);
+            this.redImage.Size = new System.Drawing.Size(52, 39);
             this.redImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.redImage.TabIndex = 0;
             this.redImage.TabStop = false;
@@ -106,18 +113,81 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(17, 368);
+            this.pictureBox1.Location = new System.Drawing.Point(36, 453);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(58, 37);
+            this.pictureBox1.Size = new System.Drawing.Size(52, 46);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.AutoSize = true;
+            this.greenLabel.Font = new System.Drawing.Font("Courier New", 10F);
+            this.greenLabel.ForeColor = System.Drawing.Color.White;
+            this.greenLabel.Location = new System.Drawing.Point(94, 470);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(69, 20);
+            this.greenLabel.TabIndex = 7;
+            this.greenLabel.Text = "label2";
+            // 
+            // teamPickPic
+            // 
+            this.teamPickPic.Image = ((System.Drawing.Image)(resources.GetObject("teamPickPic.Image")));
+            this.teamPickPic.Location = new System.Drawing.Point(12, 85);
+            this.teamPickPic.Name = "teamPickPic";
+            this.teamPickPic.Size = new System.Drawing.Size(479, 274);
+            this.teamPickPic.TabIndex = 8;
+            this.teamPickPic.TabStop = false;
+            // 
+            // awpPic
+            // 
+            this.awpPic.Image = ((System.Drawing.Image)(resources.GetObject("awpPic.Image")));
+            this.awpPic.Location = new System.Drawing.Point(36, 157);
+            this.awpPic.Name = "awpPic";
+            this.awpPic.Size = new System.Drawing.Size(213, 148);
+            this.awpPic.TabIndex = 9;
+            this.awpPic.TabStop = false;
+            // 
+            // akPic
+            // 
+            this.akPic.Image = ((System.Drawing.Image)(resources.GetObject("akPic.Image")));
+            this.akPic.Location = new System.Drawing.Point(246, 118);
+            this.akPic.Name = "akPic";
+            this.akPic.Size = new System.Drawing.Size(257, 217);
+            this.akPic.TabIndex = 10;
+            this.akPic.TabStop = false;
+            // 
+            // m4pic
+            // 
+            this.m4pic.Image = ((System.Drawing.Image)(resources.GetObject("m4pic.Image")));
+            this.m4pic.Location = new System.Drawing.Point(116, 148);
+            this.m4pic.Name = "m4pic";
+            this.m4pic.Size = new System.Drawing.Size(276, 167);
+            this.m4pic.TabIndex = 11;
+            this.m4pic.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(386, 422);
+            this.ClientSize = new System.Drawing.Size(515, 519);
+            this.Controls.Add(this.m4pic);
+            this.Controls.Add(this.akPic);
+            this.Controls.Add(this.awpPic);
+            this.Controls.Add(this.teamPickPic);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.blueLabel);
             this.Controls.Add(this.redLabel);
@@ -125,15 +195,20 @@
             this.Controls.Add(this.blueImage);
             this.Controls.Add(this.redImage);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Lost in a Forest";
+            this.Text = "CS:GO Last Round";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.blueImage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.redImage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamPickPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.awpPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.akPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m4pic)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -146,6 +221,12 @@
         private System.Windows.Forms.Label blueLabel;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.PictureBox teamPickPic;
+        private System.Windows.Forms.PictureBox awpPic;
+        private System.Windows.Forms.PictureBox akPic;
+        private System.Windows.Forms.PictureBox m4pic;
     }
 }
 
--- LostV2/Form1.cs
+++ LostV2/Form1.cs
@@ -25,9 +25,13 @@ namespace LostV2
             InitializeComponent();
 
             //display initial message and options
-            outputLabel.Text = "You find yourself in the last round of a CS:GO match. WHich side are you on?";
+            outputLabel.Text = "You find yourself in the last round of a CS:GO match. Which side are you on?";
             redLabel.Text = "Terrorist";
             blueLabel.Text = "Counter-Terrorist";
+            greenLabel.Text = "";
+            awpPic.Visible = false;
+            akPic.Visible = false;
+            m4pic.Visible = false;
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -99,90 +103,128 @@ namespace LostV2
                     }
                 }
             }
+            if (e.KeyCode == Keys.Space)       //green button press
+            {
+                if (scene == 1)
+                {
+                    scene = 14;
+                }
+                else if (scene == 4||scene == 6||scene  == 7|| scene == 8||scene == 9||scene == 11||scene == 12||scene == 13||scene == 14)
+                {
+                    scene = 0;
+                }
+            }
 
             /// Display text and game options to screen based on the current scene
             switch (scene)
             {
                 case 0:  //start scene  
-                    outputLabel.Text = "You find yourself in the last round of a CS:GO match. WHich side are you on?";
+                    outputLabel.Text = "You find yourself in the last round of a CS:GO match. Which side are you on?";
                     redLabel.Text = "Terrorist";
                     blueLabel.Text = "Counter-Terrorist";
+                    greenLabel.Text = "";
+                    teamPickPic.Visible = true;
+                    awpPic.Visible = false;
+                    akPic.Visible = false;
+                    m4pic.Visible = false;
                     break;
                 case 1:
                     outputLabel.Text = "You have no money to buy an awp. You get an M4 and armor. Do you go B site or Lower Tunnels.";
                     redLabel.Text = "B Site";
                     blueLabel.Text = "Lower Tunnels";
+                    greenLabel.Text = "CAT";
+                    teamPickPic.Visible = false;
+                    m4pic.Visible = true;
                     break;
                 case 2:
                     outputLabel.Text = "You can choose an AWP or AK with armor";
                     redLabel.Text = "AWP";
                     blueLabel.Text = "AK and Armor";
+                    greenLabel.Text = "";
+                    teamPickPic.Visible = false;
+                    awpPic.Visible = true;
+                    akPic.Visible = true;
+
                     break;
                 case 3:
                     outputLabel.Text = "Do you rush mid, or go long to get the pick";
                     redLabel.Text = "Rush Mid";
                     blueLabel.Text = "Pick Long";
+                    greenLabel.Text = "";
+                    awpPic.Visible = false;
+                    akPic.Visible = false;
                     break;
                 case 4:
-                    outputLabel.Text = "You get picked off by an awper you die";
+                    outputLabel.Text = "You get picked off by an awper you die. Your team losses";
                     redLabel.Text = "";
                     blueLabel.Text = "";
+                    greenLabel.Text = "Restart";
                     break;
                 case 5:
                     outputLabel.Text = "Your team can rush B or go CAT. Where do you go?";
                     redLabel.Text = "Rush B";
                     blueLabel.Text = "Go CAT";
+                    greenLabel.Text = "";
+                    awpPic.Visible = false;
+                    akPic.Visible = false;
                     break;
                 case 6:
                     outputLabel.Text = "You pick off an enemy peeking the doors. You continue to go long  with your team and get the bomb  down, you win.";
                     redLabel.Text = "";
                     blueLabel.Text = "";
+                    greenLabel.Text = "Restart";
                     break;
                 case 7:
                     outputLabel.Text = "You rush B and get bomb plant. The Counter Terrorists can not retake the site to defuse. You win";
                     redLabel.Text = "";
                     blueLabel.Text = "";
+                    greenLabel.Text = "Restart";
                     break;
                 case 8:
                     outputLabel.Text = "Your team gets spotted by a CT in mid and you get shot. Your team dies when they continue to push CAT and you loose";
                     redLabel.Text = "";
                     blueLabel.Text = "";
+                    greenLabel.Text = "Restart";
                     break;
                 case 9:
                     outputLabel.Text = "There is a Terrorist mid and you get taken out by him when you peak doors. You die and your team cannot hold them back. You loose.";
                     redLabel.Text = "";
                     blueLabel.Text = "";
+                    greenLabel.Text = "Restart";
                     break;
                 case 10:
                     outputLabel.Text = "You are on B site and see enemy players rushing out. Do you stay to kill them or leave to retake.";
                     redLabel.Text = "Run";
                     blueLabel.Text = "Stay";
+                    greenLabel.Text = "";
                     break;
                 case 11:
                     outputLabel.Text = "You left too late. Someone peaks B doors and head shots you running mid. You die and your team cannot retake the site without you. You lose.";
                     redLabel.Text = "";
                     blueLabel.Text = "";
+                    greenLabel.Text = "Restart";
                     break;
                 case 12:
                     outputLabel.Text = "You get taken out without killing anybody. Your team cant retake without you. You lose.";
                     redLabel.Text = "";
                     blueLabel.Text = "";
+                    greenLabel.Text = "Restart";
                     break;
                 case 13:
                     outputLabel.Text = "You get an ACE, killing the entire Terrorist team. You win the match for the team!";
                     redLabel.Text = "";
                     blueLabel.Text = "";
+                    greenLabel.Text = "Restart";
                     break;
                 case 14:
                     outputLabel.Text = "The Terrorists push CAT and take you out. They continue to the open A Site, plant the bomb and win the match. You loose.";
                     redLabel.Text = "";
                     blueLabel.Text = "";
+                    greenLabel.Text = "Restart";
                     break;
-
                 default:
                     break;
             }
         }
     }
-
 }
